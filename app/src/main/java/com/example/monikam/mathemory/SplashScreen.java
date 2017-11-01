c287fafe87920fe31c0c88dae601cc489fd03336
Monika <s160435@student.pg.edu.pl>
2017-11-01 23:56:33 +0100

    Initial commit

---
+++ app/src/main/java/com/example/monikam/mathemory/SplashScreen.java
@@ -0,0 +1,55 @@
+package com.example.monikam.mathemory;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Context currActivity = this;
+
+        Thread mySplashScreen = new Thread(){
+            public void run(){
+                try {
+                    ImageView logo = (ImageView) findViewById(R.id.imageView);
+                    Animation fadeInAnimation = AnimationUtils.loadAnimation(currActivity, R.anim.fade_in);
+                    Animation fadeOutAnimation = AnimationUtils.loadAnimation(currActivity, R.anim.fade_out);
+
+                    AnimationSet s = new AnimationSet(false);
+
+                    s.addAnimation(fadeInAnimation);
+                    s.addAnimation(fadeOutAnimation);
+                    s.setDuration(4000);
+
+                    logo.startAnimation(s);
+                    logo.setVisibility(View.INVISIBLE);
+
+                    while (s.hasEnded() == false) {
+                        sleep(10);
+                    }
+
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        mySplashScreen.start();
+    }
+}
+
+
