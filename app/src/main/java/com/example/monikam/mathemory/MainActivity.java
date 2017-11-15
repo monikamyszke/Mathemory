6ec1b27c72cee1a7b704ae0cd2e3a879e88ea0c8
Monika <s160435@student.pg.edu.pl>
2017-11-15 23:53:56 +0100

    Zmiana ikon menu głównego i dodanie czcionki

--- app/src/main/java/com/example/monikam/mathemory/MainActivity.java
+++ app/src/main/java/com/example/monikam/mathemory/MainActivity.java
@@ -1,19 +1,29 @@
 package com.example.monikam.mathemory;
 
-import android.content.DialogInterface;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.Toast;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/righteous.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
+
         setContentView(R.layout.activity_main);
+
+        ImageButton cat2 = (ImageButton) findViewById(R.id.cat2);
+        cat2.setEnabled(false);
 
         ImageButton cat3 = (ImageButton) findViewById(R.id.cat3);
         cat3.setEnabled(false);
@@ -22,9 +32,13 @@ public class MainActivity extends AppCompatActivity {
         cat4.setEnabled(false);
 
     }
+
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 }
-
-
 
 
 
